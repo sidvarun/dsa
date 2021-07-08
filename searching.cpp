3719edd6a4465936eaf8a8e024010ea94770af33
sidvarun <varunksidhwani@gmail.com>
2021-07-08 15:46:13 +0530

    new ques

--- searching.cpp
+++ searching.cpp
@@ -120,3 +120,28 @@ class Solution {
         return res;
     }
 };
+/*****************************************************************************************/
+// Find Missing And Repeating - https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/1#
+class Solution{
+public:
+    int *findTwoElement(int *arr, int n) {
+        // code here
+        int *res = new int(2);
+        
+        int i;
+        for(i = 0; i<n; i++){
+            if(arr[abs(arr[i]) - 1] > 0)
+                arr[abs(arr[i]) - 1] = arr[abs(arr[i]) - 1] * -1;
+            else
+                res[0] = abs(arr[i]);
+        }
+        for(i = 0; i<n; i++){
+            if(arr[i]>0){
+                res[1] = i+1;
+                break;
+            }
+        }
+        return res;
+    }
+};
+/********************************************************************************************/
