5b454a0e71a95b1d0117521df0ef008699d1f7d4
sidvarun <varunksidhwani@gmail.com>
2021-07-08 18:33:24 +0530

    new question

--- searching.cpp
+++ searching.cpp
@@ -145,3 +145,32 @@ public:
     }
 };
 /********************************************************************************************/
+// Majority Element - https://practice.geeksforgeeks.org/problems/majority-element-1587115620/1
+int majorityElement(int a[], int size)
+    {
+        
+        // your code here
+        
+        int res = 0;
+        int count = 1;
+        
+        for(int i = 1; i<size; i++){
+            if(a[res] != a[i])
+                count--;
+            else
+                count++;
+            if(count == 0){
+                res = i;
+                count = 1;
+            }
+        }
+        count = 0;
+        for(int i = 0; i<size; i++){
+            if(a[i] == a[res])
+                count++;
+        }
+        if(count>size/2)
+            return a[res];
+        return -1;
+    }
+/***************************************************************************************************/
