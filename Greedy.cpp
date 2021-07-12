e38e0d43d12849b5db7fd623885ce98018a524df
sidvarun <varunksidhwani@gmail.com>
2021-07-13 01:21:31 +0530

    Update Greedy.cpp

--- Greedy.cpp
+++ Greedy.cpp
@@ -259,3 +259,24 @@ public:
 };
 
 /*********************************************************************************************************/
+// Chocolate Distribution Problem - https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1
+
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+        sort(a.begin(), a.end());
+        long long int i = 0;
+        long long int j = m-1;
+        long long int minDiff = INT_MAX;
+        for(int i = 0; i < n-m+1;){
+            long long int subRes = a[j] - a[i];
+            minDiff = min(minDiff, subRes);
+            i++;
+            j++;
+        }
+        return minDiff;
+        
+    }   
+};
+/************************************************************************************************************/
