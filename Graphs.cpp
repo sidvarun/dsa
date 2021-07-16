0cec4ccce9f151b90e2da4a488800b78cd9c3e54
sidvarun <varunksidhwani@gmail.com>
2021-07-17 01:19:57 +0530

    Update Graphs.cpp

--- Graphs.cpp
+++ Graphs.cpp
@@ -642,4 +642,39 @@ class Solution
     }
 };
 /****************************************************************************/
+// Bipartite Graph - https://practice.geeksforgeeks.org/problems/bipartite-graph/1#
+class Solution {
+    public:
+        bool bfs(int s, vector<int> &color, vector<int> adj[]){
+            color[s] = 0;
+            queue<int> q;
+            q.push(s);
+            while(!q.empty()){
+                int u = q.front();
+                q.pop();
+                for(auto v : adj[u]){
+                    if(color[v] == -1){
+                        color[v] = !color[u];
+                        q.push(v);
+                    }
+                    else if(color[v] == color[u])
+                        return false;
+                }
+            }
+            return true;
+        }
+    	bool isBipartite(int V, vector<int>adj[]){
+    	    // Code here
+    	    vector<int> color(V, -1);
+    	    for(int i = 0; i<V; i++){
+    	        if(color[i] == -1){
+    	            if(!bfs(i, color, adj))
+    	                return false;
+    	        }
+    	    }
+    	    return true;
+    	}
+};
+/*************************************************************************/
+
 
