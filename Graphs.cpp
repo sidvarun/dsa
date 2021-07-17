cbb9fe0732acf1c21e93b584fd0c1b92fb988dfc
sidvarun <varunksidhwani@gmail.com>
2021-07-17 15:37:37 +0530

    Update Graphs.cpp

--- Graphs.cpp
+++ Graphs.cpp
@@ -104,6 +104,7 @@ class Solution
 };
 /*******************************************************************************************************************************/
 //Detect cycle in a Directed Graph
+// using DFS
 class Solution
 {
     public:
@@ -139,8 +140,42 @@ class Solution
         return false;
 	}
 };
+
+// using BFS
+class Solution {
+public:
+	bool isCyclic(int N, vector<int> adj[]) {
+        queue<int> q; 
+	    vector<int> indegree(N, 0); 
+	    for(int i = 0;i<N;i++) {
+	        for(auto it: adj[i]) {
+	            indegree[it]++; 
+	        }
+	    }
+	    
+	    for(int i = 0;i<N;i++) {
+	        if(indegree[i] == 0) {
+	            q.push(i); 
+	        }
+	    }
+	    int cnt = 0;
+	    while(!q.empty()) {
+	        int node = q.front(); 
+	        q.pop(); 
+	        cnt++; 
+	        for(auto it : adj[node]) {
+	            indegree[it]--;
+	            if(indegree[it] == 0) {
+	                q.push(it); 
+	            }
+	        }
+	    }
+	    if(cnt == N) return false; 
+	    return true; 
+	}
+};
 /************************************************************************************************************************/
-//Topological Sorting 
+//Topological sort - https://practice.geeksforgeeks.org/problems/topological-sort/1
 //Using BFS
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
@@ -166,7 +201,36 @@ class Solution
             } 
             return res;
 	}
-
+// Using DFS
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order.
+	void dfs(int u, stack<int> &st, vector<int> adj[], bool visited[]){
+	    visited[u] = true;
+	    for(auto v : adj[u]){
+	        if(visited[v] == false)
+	            dfs(v, st, adj, visited);
+	    }
+	    st.push(u);
+	}
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    bool visited[V] = {false};
+	    stack<int> st;
+	    vector<int> res;
+	    for(int i = 0; i<V; i++){
+	        if(visited[i] == false)
+	            dfs(i, st, adj, visited);
+	    }
+	    while(!st.empty()){
+	        res.push_back(st.top());
+	        st.pop();
+	    }
+	    return res;
+	}
+};
 /**********************************************************************************************/
 //Minimum Steps required by a knight to reach the target
 class Solution 
