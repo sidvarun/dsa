4eb6215645f1ac9fdc6c03a434c71767e6c20927
sidvarun <varunksidhwani@gmail.com>
2021-07-21 22:44:28 +0530

    Update arrays2.cpp

--- arrays2.cpp
+++ arrays2.cpp
@@ -376,3 +376,26 @@ class Solution
 
 };
 /**************************************************************************************/
+// Subarray with 0 sum  - https://practice.geeksforgeeks.org/problems/subarray-with-0-sum-1587115621/1
+
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        //Your code here
+        map<int, int> m;
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            sum += arr[i];
+            if(sum == 0)
+                return true;
+            if(m.find(sum) != m.end())
+                return true;
+            m[sum] = 1;
+        }
+        return false;
+    }
+};
+/**************************************************************************************/
