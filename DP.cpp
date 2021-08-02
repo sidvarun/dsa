0806d94f8e2c9cb0379f8fe2fb9e94fcb0ebcb63
sidvarun <varunksidhwani@gmail.com>
2021-08-02 13:32:48 +0530

    Update DP.cpp

--- DP.cpp
+++ DP.cpp
@@ -1349,5 +1349,26 @@ class Solution{
     }
 };
 /**********************************************************************************************************/
-
+// Reach a given score - https://practice.geeksforgeeks.org/problems/reach-a-given-score-1587115621/1#
+// (same as coin change - count combinations)
+long long int count(long long int n)
+{
+	long long int dp[4][n+1];
+	memset(dp, 0, sizeof(dp));
+    int val[3] = {3, 5, 10};
+    for(int i = 0; i<=3;i++)
+        dp[i][0] = 1;
+    for(int i = 0; i<=n; i++)
+        dp[0][i] = 0;
+    for(int i = 1; i<=3; i++){
+        for(int j = 1; j<=n; j++){
+            if(val[i-1]<=j)
+                dp[i][j] = dp[i][j-val[i-1]] + dp[i-1][j];
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+    return dp[3][n];
+}
+/****************************************************************************/
 
