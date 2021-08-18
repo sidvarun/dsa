719b0e59edb2b70402378c6ec5d34d55fea990fe
sidvarun <varunksidhwani@gmail.com>
2021-08-19 01:16:25 +0530

    Update LeetcodeTopInterviewQues.cpp

--- LeetcodeTopInterviewQues.cpp
+++ LeetcodeTopInterviewQues.cpp
@@ -934,3 +934,20 @@ public:
     }
 };
 /*************************************************************/
+// 49. Group Anagrams - https://leetcode.com/problems/group-anagrams/
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        vector<string> temp = strs;
+        map<string, vector<string>> m;
+        for(int i = 0; i<temp.size(); i++){
+            sort(temp[i].begin(), temp[i].end());
+            m[temp[i]].push_back(strs[i]);
+        }
+        for(auto v = m.begin(); v != m.end(); v++)
+            res.push_back(v->second);
+        return res;
+    }
+};
+/****************************************************************/
