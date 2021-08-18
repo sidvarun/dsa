130ee7bd89c15d87b8586eace6f7aa0a6ca2429e
sidvarun <varunksidhwani@gmail.com>
2021-08-19 01:50:56 +0530

    Update LeetcodeTopInterviewQues.cpp

--- LeetcodeTopInterviewQues.cpp
+++ LeetcodeTopInterviewQues.cpp
@@ -952,3 +952,26 @@ public:
     }
 };
 /****************************************************************/
+// Implement pow(x, n), which calculates x raised to the power n (i.e., xn). - https://leetcode.com/problems/powx-n/
+class Solution {
+public:
+    double myPow(double x, int n) {
+        bool sign = false;
+        if(n < 0)
+            sign = true;
+        long long power = abs(n);
+        double res = 1;
+        while(power > 0){
+            if(power % 2 != 0){
+                res = res * x;
+                power--;
+            }
+            else{
+                x = x * x;
+                power /= 2;
+            }
+        }
+        return sign ? (double)1.0 / res : (double) res;
+    }
+};
+/******************************************************************/
