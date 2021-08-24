2353989e3d4c9210ebd0437ff7c96dd94e8d7264
Varun Kumar <varunksidhwani@gmail.com>
2021-08-24 11:36:46 -0700

    Create pepcodingDp.java

---
+++ pepcodingDp.java
@@ -0,0 +1,230 @@
+//13 . Gold Mine Problem
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+   private static class Pair {
+      String psf;
+      int i;
+      int j;
+
+      public Pair(String psf, int i, int j) {
+         this.psf = psf;
+         this.i = i;
+         this.j = j;
+      }
+   }
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      int m = Integer.parseInt(br.readLine());
+      int[][] arr = new int[n][m];
+
+      for (int i = 0; i < n; i++) {
+         String str = br.readLine();
+         for (int j = 0; j < m; j++) {
+            arr[i][j] = Integer.parseInt(str.split(" ")[j]);
+         }
+      }
+
+      int[][] dp = new int[arr.length][arr[0].length];
+      
+      for(int j = arr[0].length - 1; j >= 0; j--){
+          for(int i = 0; i < arr.length; i++){
+              if(j == arr[0].length - 1){
+                  dp[i][j] = arr[i][j];
+              } else if(i == 0){
+                  dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i + 1][j + 1]);
+              } else if(i == arr.length - 1){
+                  dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i - 1][j + 1]);
+              } else {
+                  dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], Math.max(dp[i - 1][j + 1], dp[i + 1][j + 1]));
+              }
+          }
+      }
+      
+      int max = Integer.MIN_VALUE;
+      for(int i = 0; i < dp.length; i++){
+          if(dp[i][0] > max){
+              max = dp[i][0];
+          }
+      }
+      
+      System.out.println(max);
+      
+      ArrayDeque<Pair> que = new ArrayDeque<>();
+      
+      for(int i = 0; i < dp.length; i++){
+          if(dp[i][0] == max){
+              que.add(new Pair(i + " ", i, 0));
+          }
+      }
+      
+      while(que.size() > 0){
+          Pair rem = que.removeFirst();
+          
+          if(rem.j == arr[0].length - 1){
+              System.out.println(rem.psf);
+          } else if(rem.i == 0){
+              int g = Math.max(dp[rem.i][rem.j + 1], dp[rem.i + 1][rem.j + 1]);
+              
+              
+              if(g == dp[rem.i][rem.j + 1]){
+                  que.add(new Pair(rem.psf + "d2 ", rem.i, rem.j + 1));
+              }
+              
+              if(g == dp[rem.i + 1][rem.j + 1]){
+                  que.add(new Pair(rem.psf + "d3 ", rem.i + 1, rem.j + 1));
+              }
+          } else if(rem.i == arr.length - 1){
+              int g = Math.max(dp[rem.i][rem.j + 1], dp[rem.i - 1][rem.j + 1]);
+              
+              
+              if(g == dp[rem.i - 1][rem.j + 1]){
+                  que.add(new Pair(rem.psf + "d1 ", rem.i - 1, rem.j + 1));
+              }
+              
+              if(g == dp[rem.i][rem.j + 1]){
+                  que.add(new Pair(rem.psf + "d2 ", rem.i, rem.j + 1));
+              }
+          } else {
+              int g = Math.max(dp[rem.i][rem.j + 1], Math.max(dp[rem.i - 1][rem.j + 1], dp[rem.i + 1][rem.j + 1]));
+              
+              if(g == dp[rem.i - 1][rem.j + 1]){
+                  que.add(new Pair(rem.psf + "d1 ", rem.i - 1, rem.j + 1));
+              }
+              
+              if(g == dp[rem.i][rem.j + 1]){
+                  que.add(new Pair(rem.psf + "d2 ", rem.i, rem.j + 1));
+              }
+              
+              if(g == dp[rem.i + 1][rem.j + 1]){
+                  que.add(new Pair(rem.psf + "d3 ", rem.i + 1, rem.j + 1));
+              }
+          }
+          
+          
+      }
+   }
+
+
+}
+/******************************************************************************************************/
+// 14 . Print Subsets with target sum
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static class Pair{
+        int i;
+        int j;
+        String psf;
+
+        public Pair(int i, int j, String psf){
+            this.i = i;
+            this.j = j;
+            this.psf = psf;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int tar = Integer.parseInt(br.readLine());
+
+        boolean[][] dp = new boolean[arr.length + 1][tar + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0) {
+                    dp[i][j] = false;
+                } else if (j == 0) {
+                    dp[i][j] = true;
+                } else {
+                    if(dp[i - 1][j] == true){
+                        dp[i][j] = true;
+                    } else {
+                        int val = arr[i - 1];
+                        if (j >= val && dp[i - 1][j - val] == true) {
+                            dp[i][j] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[dp.length - 1][tar]);
+        Queue<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(n,tar,""));
+      
+
+        while (q.size() > 0) {
+			Pair rp = q.remove();
+			if (rp.i == 0 || rp.j == 0) {
+				System.out.println(rp.psf);
+			} else {
+				boolean exc = dp[rp.i - 1][rp.j];
+				boolean inc = rp.j - arr[rp.i - 1] >= 0 ? dp[rp.i - 1][rp.j - arr[rp.i - 1]] : false;
+				
+				if (inc == true) {
+					q.add(new Pair(rp.i - 1, rp.j - arr[rp.i - 1], (rp.i - 1) + " " + rp.psf));
+				}
+				if (exc == true) {
+					q.add(new Pair(rp.i - 1, rp.j, rp.psf));
+				}
+			}
+		}
+    }
+}
+
+//16 . Longest Palisdromic Subsequence
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        
+        int[][] dp = new int[str.length()][str.length()];
+        for(int g = 0; g < dp.length; g++){
+            for(int i = 0, j = g; j < dp[0].length; i++, j++){
+                if(g == 0){
+                    dp[i][j] = 1;
+                } else if(g == 1){
+                    dp[i][j] = str.charAt(i) == str.charAt(j)? 2: 1;
+                } else {
+                    if(str.charAt(i) == str.charAt(j)){
+                        dp[i][j] = 2 + dp[i + 1][j - 1];
+                    } else {
+                        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[0][dp[0].length - 1]);
+    }
+}
+
+
+                        
+                        
+
+
+                        
+
+
+                        
+
+
+                        
