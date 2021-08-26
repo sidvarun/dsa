222f5180b315c144df3d09afbfaa884166bd5a58
sidvarun <varunksidhwani@gmail.com>
2021-08-26 19:14:24 +0530

    Update pepcodingDp.java

--- pepcodingDp.java
+++ pepcodingDp.java
@@ -595,6 +595,167 @@ public class Main {
 	}
 
 }
+/********************************************************************************************************************/
+// 33. perfect Squares
+class Solution {
+    public:
+        int numSquares(int n) {
+            if (n <= 0)
+            {
+                return 0;
+            }
+
+            // cntPerfectSquares[i] = the least number of perfect square numbers 
+            // which sum to i. Note that cntPerfectSquares[0] is 0.
+            vector<int> cntPerfectSquares(n + 1, INT_MAX);
+            cntPerfectSquares[0] = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                // For each i, it must be the sum of some number (i - j*j) and 
+                // a perfect square number (j*j).
+                for (int j = 1; j*j <= i; j++)
+                {
+                    cntPerfectSquares[i] = 
+                        min(cntPerfectSquares[i], cntPerfectSquares[i - j*j] + 1);
+                }
+            }
+
+            return cntPerfectSquares.back();
+        }
+};
+/**********************************************************************************************************************/
+// 35. Kadane's algorithm
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+
+    public static int solution(int[] arr) {
+        int finalans = arr[0];
+        int curr = arr[0];
+        for(int i = 1 ; i < arr.length; i++){
+            curr = Math.max(arr[i], arr[i] + curr);
+            finalans = Math.max(finalans, curr);
+        }
+        return finalans; 
+    }
+}
+
+/************************************************************************************************************************/
+// 35. Numeric Keypad
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static int solution(int n) {
+		ArrayList<Integer>[] nextkeys = new ArrayList[10];
+		for(int i = 0; i < 10; i++){
+		    nextkeys[i] = new ArrayList<>();
+		}
+		
+		nextkeys[0].add(0);
+		nextkeys[0].add(8);
+		nextkeys[1].add(1);
+		nextkeys[1].add(2);
+		nextkeys[1].add(4);
+		nextkeys[2].add(1);
+		nextkeys[2].add(2);
+		nextkeys[2].add(3);
+		nextkeys[2].add(5);
+		nextkeys[3].add(2);
+		nextkeys[3].add(3);
+		nextkeys[3].add(6);
+		nextkeys[4].add(1);
+		nextkeys[4].add(4);
+		nextkeys[4].add(5);
+		nextkeys[4].add(7);
+		nextkeys[5].add(2);
+		nextkeys[5].add(4);
+		nextkeys[5].add(5);
+		nextkeys[5].add(6);
+		nextkeys[5].add(8);
+		nextkeys[6].add(3);
+		nextkeys[6].add(5);
+		nextkeys[6].add(6);
+		nextkeys[6].add(9);
+		nextkeys[7].add(4);
+		nextkeys[7].add(7);
+		nextkeys[7].add(8);
+		nextkeys[8].add(0);
+		nextkeys[8].add(5);
+		nextkeys[8].add(7);
+		nextkeys[8].add(8);
+		nextkeys[8].add(9);
+		nextkeys[9].add(6);
+		nextkeys[9].add(8);
+		nextkeys[9].add(9);
+		
+		int[][] dp = new int[n + 1][10];
+		for(int i = 0 ; i < 10; i++){
+		    dp[1][i] = 1;
+		}
+		
+		for(int i = 2; i <= n; i++){
+		    for(int j = 0; j < 10; j++){
+		        for(int key = 0 ; key < nextkeys[j].size();key++){
+		            dp[i][j] += dp[i - 1][nextkeys[j].get(key)]; 
+		        }
+		    }
+		}
+		
+		int ans = 0;
+		for(int j = 0 ; j < 10; j++){
+		    ans += dp[n][j];
+		}
+		return ans;
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println(solution(scn.nextInt()));
+	}
+
+}
+/******************************************************************************************************/
+// Longest Common Substring
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String s1 = scn.next();
+		String s2 = scn.next();
+		System.out.println(solution(s1,s2));
+	}
+
+	public static int solution(String s1, String s2){
+		int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+		int ans = 0 ;
+		for(int i = 1 ; i <= s1.length(); i++) {
+			for(int j = 1 ; j <= s2.length() ;j++) {
+				if(s1.charAt(i - 1) == s2.charAt(j - 1)) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				}
+				ans = Math.max(ans, dp[i][j]);
+			}
+		}
+		return ans;
+	}
+
+}
+/************************************************************************************************************/
 
                         
 
