f4b937b3bbb3194ee432beb93131853da5ac0f0c
sidvarun <varunksidhwani@gmail.com>
2021-08-28 22:52:45 +0530

    ff

---
+++ sample.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long int solve(int n, int s, vector<int> stock, vector<int> price, vector<int> delivery){
+    if(n == 0)
+        return 0;
+    if(s == 0)
+        return INT_MAX;
+    long long int res = INT_MAX;
+    for(int i = 0; i<=stock[s - 1] && i<=n; i++){
+        
+        long long int temp = i * price[s - 1] + delivery[s - 1] + solve(n - i, s - 1, stock, price, delivery);
+        if(temp<res)
+            res = temp;
+    }
+    return res;
+}
+
+int main(){
+    int n = 10;
+    int s = 2;
+    vector<int> stock = {5, 1000};
+    vector<int> price = {5, 10};
+    vector<int> delivery = {50, 0};
+    long long int ans = solve(n, s, stock, price, delivery);
+    cout<<ans;
+    return 0;
+}
+if(i > 0)
+             temp = temp + delivery[s - 1];
+
+#include<bits/stdc++.h>
+using namespace std;
+long long int solve(int n, int s, vector<int> stock, vector<int> price, vector<int> delivery){
+    if(n == 0)
+        return 0;
+    if(s == 0)
+        return INT_MAX;
+    long long int res = INT_MAX;
+    for(int i = 0; i<=stock[s - 1] && i<=n; i++){
+        
+        long long int temp = i * price[s - 1] + delivery[s - 1] + solve(n - i, s - 1, stock, price, delivery);
+        if(temp<res)
+            res = temp;
+    }
+    return res;
+}
+
+int main(){
+    int n = 20;
+    int s = 4;
+    vector<int> stock = {5, 10, 15, 20};
+    vector<int> price = {5, 4, 6, 7};
+    vector<int> delivery = {6, 12, 9, 0};
+    long long int ans = solve(n, s, stock, price, delivery);
+    cout<<ans;
+    return 0;
+}
