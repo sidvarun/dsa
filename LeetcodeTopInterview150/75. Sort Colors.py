995bff13768b8f1de50d0045de5238622001ff9e
sidvarun <varunksidhwani@gmail.com>
2021-10-02 11:37:01 +0530

    Update 75. Sort Colors.py

--- LeetcodeTopInterview150/75. Sort Colors.py
+++ LeetcodeTopInterview150/75. Sort Colors.py
@@ -23,3 +23,24 @@ def sortColors(self, nums):
         else:
             nums[white], nums[blue] = nums[blue], nums[white]
             blue -= 1
+
+# My AC code
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = -1, j = 0, k = nums.size();
+        while(j < k){
+            if(nums[j] == 0){
+                i++;
+                swap(nums[j], nums[i]);
+                j++;
+            }
+            else if(nums[j] == 1)
+                j++;
+            else{
+                k--;
+                swap(nums[j], nums[k]);
+            }
+        }
+    }
+};
