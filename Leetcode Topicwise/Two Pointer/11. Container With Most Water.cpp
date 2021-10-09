c4c675cfa38f76d43781e4693d6cbc8b732941ff
sidvarun <varunksidhwani@gmail.com>
2021-10-09 10:30:41 +0530

    f

---
+++ Leetcode Topicwise/Two Pointer/11. Container With Most Water.cpp
@@ -0,0 +1,19 @@
+https://leetcode.com/problems/container-with-most-water/
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size() - 1;
+        int left = 0;
+        int right = n;
+        int res = INT_MIN;
+        while(left < right){
+            res = max(res, min(height[left], height[right]) * (right - left));
+            if(height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return res;
+    }
+};
