7ba2cd2d5baef6178af53d070d4388671959c94a
sidvarun <varunksidhwani@gmail.com>
2021-11-01 12:22:31 +0530

    dfs

--- Leetcode Topicwise/TopologicalSorting/802. Find Eventual Safe States.cpp
+++ Leetcode Topicwise/TopologicalSorting/802. Find Eventual Safe States.cpp
@@ -36,3 +36,39 @@ public:
 
 // using dfs
 
+class Solution {
+public:
+    unordered_set<int> safe;
+    unordered_set<int> cycle;
+    
+    bool dfs(vector<vector<int>> &graph, vector<int> visited, int src){
+        if(safe.find(src) != safe.end())
+            return true;
+        if(cycle.find(src) != cycle.end())
+            return false;
+        if(visited[src] == 1){
+            cycle.insert(src);
+            return false;
+        }
+        visited[src] = 1;
+        for(auto v : graph[src]){
+            if(!dfs(graph, visited, v)){
+                cycle.insert(src);
+                return false;
+            }
+        }
+        safe.insert(src);
+        return true;
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int> res;
+        int n = graph.size();
+        vector<int> visited(n, 0);
+        for(int i = 0; i<n; i++){
+            if(dfs(graph, visited, i))
+                res.push_back(i);
+        }
+        return res;
+    }
+};
