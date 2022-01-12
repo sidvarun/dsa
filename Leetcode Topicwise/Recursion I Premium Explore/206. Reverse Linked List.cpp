969c6c13f9ada9a30acc527aab8df054e6b29b7c
Varun Kumar <varunksidhwani@gmail.com>
2022-01-12 13:37:51 +0530

    206

---
+++ Leetcode Topicwise/Recursion I Premium Explore/206. Reverse Linked List.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return head;
+        ListNode* prevHead = head;
+        ListNode* tail = head->next;
+        head = reverseList(head->next);
+        tail->next = prevHead;
+        prevHead->next = NULL;
+        return head;
+    }
+};
+
+/****************************************************************/
+
+public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) return head;
+    ListNode p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
+}
