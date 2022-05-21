a27e189bb34fb996daf113acff2452810f642a90
sidvarun <varunksidhwani@gmail.com>
2022-05-21 23:35:41 +0530

    1260

---
+++ Finale/1260. Shift 2D Grid.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<vector<int>> res(m, vector<int>(n, 0));
+        
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                int index = ((i * n + j) + k) % (m * n);
+                
+                res[index / n][index % n] = grid[i][j];               
+            }
+        }
+        
+        return res;
+    }
+};
